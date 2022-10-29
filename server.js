52b6a370109e5f93611e8110b9be380b0dc25ea8
Nadagani Amulya Prasanth <75437956+amulyaprasanth@users.noreply.github.com>
2022-10-29 13:06:18 +0530

    added scripts

--- server.js
+++ server.js
@@ -27,24 +27,26 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-    signin.handleSignin(req, res, db, bcrypt)});
+    signin.handleSignin(req, res, db, bcrypt);
+});
 
 app.post("/register", (req, res) => {
     register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
-    profile.handleProfileGet(req, res, db)
+    profile.handleProfileGet(req, res, db);
 });
 
 app.put("/image", (req, res) => {
-    image.handleImage(req, res, db)
+    image.handleImage(req, res, db);
 });
 
 app.post("/imageurl", (req, res) => {
-    image.handleApiCall(req, res)
-})
+    image.handleApiCall(req, res);
+});
 
-app.listen(3000, () => {
-    console.log("app is running on port 3000");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 });
